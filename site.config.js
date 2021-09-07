c6e05a3eb8ccd43f7bbf60499885c75b35c69d45
yanglele <youngle316@gmail.com>
2021-09-07 17:02:06 +0800

    feat: update page config

--- site.config.js
+++ site.config.js
@@ -12,14 +12,13 @@ module.exports = {
   author: '杨乐乐',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: 'YoungLe Blog',
+  socialImageTitle: 'YoungLe',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
   twitter: 'youngle316',
   github: 'youngle316',
-  linkedin: 'youngle316',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -32,7 +31,7 @@ module.exports = {
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: null,
+  utterancesGitHubRepo: 'youngle316/nextjs-notion-starter-kit',
 
   // whether or not to enable support for LQIP preview images (optional)
   // NOTE: this requires you to set up Google Firebase and add the environment
