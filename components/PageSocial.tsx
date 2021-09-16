6fad281e20074845e6d3bb430d162ee5d26bed07
yanjiuqi <90023587+yanjiuqi@users.noreply.github.com>
2021-09-16 16:08:24 +0800

    Update PageSocial.tsx

--- components/PageSocial.tsx
+++ components/PageSocial.tsx
@@ -18,7 +18,7 @@ const socialLinks: SocialLink[] = [
     href: `https://www.instagram.com/syc512/`,
     title: `instagram @${config.twitter}`,
     icon: (
-      <img src=require("./ins.svg") alt="img" />
+      <img src="https://t.focus-res.cn/front-end/applet/static/3d/hx_a.jpg" alt="img" />
     )
   },
 
